45db1a01ebf4efbb4d560d3763d473ac1b2a7b16
glen-s-abraham <glenprojects666@gmail.com>
2022-04-30 11:50:51 +0530

    abstract level branch traversal

---
+++ traversal.js
@@ -0,0 +1,55 @@
+const jsnx = require('jsnetworkx')
+
+const graph = new jsnx.Graph()
+
+graph.addNode('A')
+graph.addNode('B')
+graph.addNode('C')
+graph.addNode('D')
+graph.addNode('E')
+graph.addNode('F')
+graph.addNode('G')
+graph.addNode('H')
+graph.addNode('I')
+graph.addNode('J')
+graph.addNode('K')
+graph.addNode('L')
+
+graph.addEdge('A','B')
+graph.addEdge('B','C')
+graph.addEdge('C','D')
+graph.addEdge('D','E')
+graph.addEdge('E','F')
+graph.addEdge('F','G')
+graph.addEdge('G','H')
+graph.addEdge('D','I')
+graph.addEdge('I','J')
+graph.addEdge('D','K')
+graph.addEdge('K','L')
+
+const unvisitedMatrix = {}
+const visitedMatrix={}
+graph.nodes().forEach(node=>{visitedMatrix[node]=false})
+
+const path = []
+
+chaeckIfAllNodesAreVisited=()=>{
+    let isComplete=true
+    Object.values(visitedMatrix).forEach(val=>{if(!val){isComplete=false};})
+    return isComplete
+}
+
+graph.nodes().forEach(node=>{
+    if((graph.degree(node)-1)>1){
+        let neighbors = graph.neighbors(node)
+        neighbors = neighbors.filter(node=>visitedMatrix[node]==false)
+        console.log(neighbors)
+        unvisitedMatrix[node]=neighbors
+    }
+    console.log(chaeckIfAllNodesAreVisited())
+    visitedMatrix[node]=true
+    
+})
+
+console.log(unvisitedMatrix)
+
