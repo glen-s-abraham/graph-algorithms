5878731f7e320ff7a5994079f9d979095a15d789
glen-s-abraham <glenprojects666@gmail.com>
2022-05-04 12:17:26 +0530

    backtrackandprune refactored

--- backrtackAndPrune.js
+++ backrtackAndPrune.js
@@ -8,29 +8,36 @@ const sheaths=[
 ]
 const branchedVertices = ['C']
 
-const cleanRepeatingStrands=(sheaths)=>{
-    pairs = []
-    for(let i=0;i<sheaths.length;i++){
-        let segments = sheaths[i]
-        while(sheaths[i].length>1){
-            let v1=segments.shift()
-            let v2 = segments[0]
+const pairVerticesWithinSheeths=(sheath)=>{
+    let _sheaths=[]
+    sheath.forEach(sheath=>{
+        let pairs=[]
+        while(sheath.length>1){
+            let v1=sheath.shift()
+            let v2=sheath[0]
             pairs.push([v1,v2])
         }
-    } 
-    for(let i=pairs.length-1;i>0;i--){
+        _sheaths.push(pairs)
+    })
+    return _sheaths
+}
+
+const cleanRepeatingStrands=(sheaths)=>{
+    let pairs = []
+    for(let i=sheaths.length-1;i>0;i--){
         for(let j=i-1;j>=0;j--){
-            if(
-                isArrayEqual(pairs[i],pairs[j].slice().reverse())||
-                isArrayEqual(pairs[i],pairs[j])
-            ){
-                console.log(pairs[i],pairs[j])
-                console.log(getIndexOfPair(pairs,pairs[j]))
-                pairs.splice(getIndexOfPair(pairs,pairs[i]),1)
-            }    
+            sheaths[i].forEach(iSegment=>{
+                sheaths[j].forEach(jSegment=>{
+                    if(isArrayEqual(iSegment,jSegment)||isArrayEqual(iSegment,jSegment.slice().reverse())){
+                        console.log('repeating segment',iSegment)
+                        let segmentIndex=getIndexOfPair(sheaths[i],iSegment)
+                        sheaths[i].splice(segmentIndex,1)
+                    }
+                })
+            })
         }
     }
-    console.log(pairs)
+    console.log(sheaths)
 }
 
 
@@ -46,7 +53,11 @@ const getIndexOfPair=(array,pair)=>{
             return i
     return -1
 }
-cleanRepeatingStrands([...sheaths])
+
+
+const pairedSheaths=pairVerticesWithinSheeths([...sheaths])
+cleanRepeatingStrands([...pairedSheaths])
+// console.log(pairedSheaths)
 
 
 
